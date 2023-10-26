29287be6134ca0b5ccccc355b381ccae7702b2a1
Anna Maria <aannamariabr@outlook.com>
2023-10-26 13:48:57 -0300

    dynamic route with more then one url param

---
+++ src/app/catalog/product/[...data]/page.tsx
@@ -0,0 +1,21 @@
+interface ProductProps {
+  params: {
+    data: string[]
+  }
+}
+
+export default function Product({ params }: ProductProps) {
+  const [productId, size, color] = params.data
+
+  // http://localhost:3000/catalog/product/40028922/small/purple
+  // Product ID: 40028922
+  // Size: small
+  // Color: purple
+  return (
+    <div>
+      <p>Product ID: {productId}</p>
+      <p>Size: {size}</p>
+      <p>Color: {color}</p>
+    </div>
+  )
+}
--- src/app/catalog/product/[id]/page.tsx
+++
@@ -1,9 +0,0 @@
-interface ProductProps {
-  params: {
-    id: string
-  }
-}
-
-export default function Product(props: ProductProps) {
-  return <h1>{JSON.stringify(props.params.id)}</h1>
-}
