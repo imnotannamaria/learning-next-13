cd86be0e266837ec3bef0e2cc9ab67ece02bf67a
Anna Maria <aannamariabr@outlook.com>
2023-10-27 13:22:53 -0300

    Client boundaries and thread

--- src/app/catalog/product/[...data]/add-to-card-button.tsx
+++ src/app/catalog/product/[...data]/add-to-card-button.tsx
@@ -1,6 +1,7 @@
 'use client'
 
 import { useState } from 'react'
+import { Thread } from './thread'
 
 export function AddToCardButton() {
   const [count, setCount] = useState(0)
@@ -9,5 +10,10 @@ export function AddToCardButton() {
     setCount((state) => state + 1)
   }
 
-  return <button onClick={addToCard}>Adicionar ao carrinho ({count})</button>
+  return (
+    <button onClick={addToCard}>
+      Adicionar ao carrinho ({count})
+      <Thread />
+    </button>
+  )
 }
---
+++ src/app/catalog/product/[...data]/thread.tsx
@@ -0,0 +1,11 @@
+export function Thread() {
+  return (
+    <h1
+      onClick={() => {
+        console.log('thread')
+      }}
+    >
+      Thread
+    </h1>
+  )
+}
